b0a28dbfcc01bf00eebe18e9e8a576b4008b0a78
yuval <yuvalatia1995@gmail.com>
2020-06-30 21:41:09 +0300

    first look of the project - logo + fonts

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 
     var synth = new Tone.PolySynth().toMaster(); // Initialize the OSC object
 
-
+      let keysByOrder = [['Q','2','W','3','E','R','5','T','6','Y','7','U']
+                        ,['I','9','O','0','P','V','G','B','H','N','J','M']];
       var notes = ["C", "D", "E", "F", "G", "A", "B"]; // The notes for the keyboard
+
       var html = ""; // after the next for loop this var will be the content for the main container
+      let counter = 0;
       for (var octave = 0; octave < 2; octave++) {
         for (var i = 0; i < notes.length; i++) {
           var note = notes[i];
           var hasSharp = true;
-
+          
           if (note == "E" || note == "B") hasSharp = false; // unneccesary black notes
 
           html += `<div class='whitenote'
           onmousedown='noteDown(this,false)'
           onmouseup='noteUp(this,false)'
           onmouseleave='noteUp(this,false')
-           data-note='${note + (octave + 4)}'><span>ff</span>`;
+           data-note='${note + (octave + 4)}'><span>${keysByOrder[octave][counter]}</span>`;
 
           if (hasSharp) {
+            counter++;
             html += `<div class='blacknote'
             onmousedown='noteDown(this,true)'
             onmouseup='noteUp(this,true)'
             onmouseleave='noteUp(this,true')   
-            data-note='${note + "#" + (octave + 4)}'><span>ff</span></div>`;
+            data-note='${note + "#" + (octave + 4)}'><span>${keysByOrder[octave][counter]}</span></div>`;
           }
 
           
 
           html += "</div>";
+
+          counter++;
         }
+        counter=0;
       }
 
       document.getElementById("container").innerHTML = html; // put the content on the main container
